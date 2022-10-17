882957f63e23b642960c130e6d7955f814160f49
matiasecharri <matiasecharri@hotmail.com>
2022-10-17 20:03:38 -0300

    agregando filtros de busqueda

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+//HACER APARECER CARTAS--------------------------------------------------------
+
 let container = document.getElementById("cartaid");
 for (i = 0; i < events.length; i++) {
-  let cartas = document.createElement("div");
-  cartas.className = "card";
-  cartas.innerHTML += ` <img src="${events[i].image}" class="card-img-top" alt="${events[i].name}">
+  container.innerHTML += ` 
+  <div class="card">
+  <img src="${events[i].image}" class="card-img-top" alt="${events[i].name}">
   <div class="card-body-d-flex align-items-around">
     <h5 class="card-title">${events[i].name}
   </h5><div class="cuadrado">
@@ -12,25 +14,57 @@ for (i = 0; i < events.length; i++) {
     <div class="botones">
     <a href="./details.html" class="btn">Know more info</a>
     <div/>
+  </div>
   </div>`;
-  container.appendChild(cartas);
 }
 
-let checkbox = document.getElementById("checkboxbar");
-let categories = new Set(
+//HACER APARECER CHECKBOX --------------------------------------------------------///////////////////////////////////////////////////////////////////////////////////////////////
+
+let checkbox = document.getElementById("checkboxbar"); //Traje el contenedor padre, para que los checkbox queden dentro de el.
+let arrayMapeadoDeEventos = new Set( //Creo una nueva variable la cual es igual a un array, usando MAP me saco de encima la información que no me sirve y como la tengo duplicada uso new Set
   events.map(function (i) {
-    return i.category;
+    return i.category; //Retorno el i.category que es lo que me interesa sacar del MAP.
   })
 );
 
-function impressCheck(categoria) {
+function impressCheck(stringQueSepareArriba) {
   checkbox.innerHTML += `<div class="form-check">
-<input class="form-check-input" type="checkbox" value="${categoria}" id="flexCheckDefault">
+<input class="form-check-input" type="checkbox" value="${stringQueSepareArriba}" id="flexCheckDefault">
 <label class="form-check-label" for="flexCheckDefault">
-  ${categoria}
+  ${stringQueSepareArriba}
 </label>
 </div>
 `;
 }
 
-categories.forEach(impressCheck);
+arrayMapeadoDeEventos.forEach(impressCheck);
+
+// EVENTOS -------------------------------------------------------------------------
+
+let search = document.getElementById("buscando");
+
+search.addEventListener("change", (evento) => {
+  let textFilter = evento.target.value;
+
+  let eventTextFiltered = events.filter((evento) =>
+    evento.name.toLowerCase().includes(textFilter.toLowerCase())
+  );
+  container.innerHTML = " ";
+
+  for (evento of eventTextFiltered) {
+    container.innerHTML += ` 
+    <div class="card">
+    <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
+    <div class="card-body-d-flex align-items-around">
+      <h5 class="card-title">${evento.name}
+    </h5><div class="cuadrado">
+      <p class="card-text">${evento.description}</p>
+      </div>
+     
+      <div class="botones">
+      <a href="./details.html" class="btn">Know more info</a>
+      <div/>
+    </div>
+    </div>`;
+  }
+});
--- pastevents.js
+++ pastevents.js
@@ -1,9 +1,9 @@
 let container = document.getElementById("cartaid");
 for (i = 0; i < events.length; i++) {
-       if (events[i].date < currentDate){
-  let cartas = document.createElement("div");
-  cartas.className = "card";
-  cartas.innerHTML += ` <img src="${events[i].image}" class="card-img-top" alt="${events[i].name}">
+  if (events[i].date < currentDate) {
+    container.innerHTML += ` 
+  <div class="card">
+  <img src="${events[i].image}" class="card-img-top" alt="${events[i].name}">
   <div class="card-body-d-flex align-items-around">
     <h5 class="card-title">${events[i].name}
   </h5><div class="cuadrado">
@@ -13,27 +13,60 @@ for (i = 0; i < events.length; i++) {
     <div class="botones">
     <a href="./details.html" class="btn">Know more info</a>
     <div/>
-  </div>`;   container.appendChild(cartas);
-       }
+  </div>
+  </div>`;}
 }
 
+//HACER APARECER CHECKBOX --------------------------------------------------------///////////////////////////////////////////////////////////////////////////////////////////////
 
-
-let checkbox = document.getElementById("checkboxbar");
-let categories = new Set(
+let checkbox = document.getElementById("checkboxbar"); //Traje el contenedor padre, para que los checkbox queden dentro de el.
+let arrayMapeadoDeEventos = new Set( //Creo una nueva variable la cual es igual a un array, usando MAP me saco de encima la información que no me sirve y como la tengo duplicada uso new Set
   events.map(function (i) {
-    return i.category;
+    return i.category; //Retorno el i.category que es lo que me interesa sacar del MAP.
   })
 );
 
-function impressCheck(categoria) {
+function impressCheck(stringQueSepareArriba) {
   checkbox.innerHTML += `<div class="form-check">
-<input class="form-check-input" type="checkbox" value="${categoria}" id="flexCheckDefault">
+<input class="form-check-input" type="checkbox" value="${stringQueSepareArriba}" id="flexCheckDefault">
 <label class="form-check-label" for="flexCheckDefault">
-  ${categoria}
+  ${stringQueSepareArriba}
 </label>
 </div>
 `;
 }
 
-categories.forEach(impressCheck);
+arrayMapeadoDeEventos.forEach(impressCheck);
+
+// EVENTOS -------------------------------------------------------------------------
+
+let search = document.getElementById("buscando");
+
+search.addEventListener("change", (evento) => {
+  let textFilter = evento.target.value;
+
+  let eventTextFiltered = events.filter((evento) =>
+    evento.name.toLowerCase().includes(textFilter.toLowerCase())
+  );
+  container.innerHTML = " ";
+
+  for (i = 0; i < eventTextFiltered.length; i++) {
+    if (eventTextFiltered[i].date < currentDate) {
+      container.innerHTML += ` 
+    <div class="card">
+    <img src="${eventTextFiltered[i].image}" class="card-img-top" alt="${eventTextFiltered[i].name}">
+    <div class="card-body-d-flex align-items-around">
+      <h5 class="card-title">${eventTextFiltered[i].name}
+    </h5><div class="cuadrado">
+      <p class="card-text">${eventTextFiltered[i].description}</p>
+      </div>
+     
+      <div class="botones">
+      <a href="./details.html" class="btn">Know more info</a>
+      <div/>
+    </div>{
+    </div>`;}
+
+  }
+});
+
