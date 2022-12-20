fa1cac5f394c6fc94e4e60dfc1594f9c62a916b7
matiasecharri <matiasecharri@hotmail.com>
2022-12-20 01:33:43 -0300

    up

--- events.js
+++ events.js
@@ -1,178 +1,187 @@
-
- let  currentDate = "2022-01-01"
- let events = [
-    {
-      _id: 1,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Feriadecomidas7.jpg",
-      "name":"Collectivities Party",
-      "date":"2021-12-12",
-      "description":"Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-      "category":"Food Fair",
-      "place":"Room A",
-      "capacity":45000,
-      "assistance":42756,
-      "price":5
-    },
-    {
-      _id: 2,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Feriadecomidas2.jpg",
-      "name":"Korean style",
-      "date":"2022-08-12",
-      "description":"Enjoy the best Korean dishes, with international chefs and awesome events.",
-      "category":"Food Fair",
-      "place":"Room A",
-      "capacity":45000,
-      "assistance":42756,
-      "price":10
-    },
-    {
-      _id: 3,
-      "image":"https://s3-us-west-2.amazonaws.com/montana-surveytoolimages/general/full-size/635905468279528880.jpg",
-      "name":"Jurassic Park",
-      "date":"2021-11-02",
-      "description":"Let's go meet the biggest dinosaurs in the paleontology museum.",
-      "category":"Museum",
-      "place":"Field",
-      "capacity":82000,
-      "assistance":65892,
-      "price":15
-    },
-    {
-      _id: 4,
-      "image":"https://wallpaperaccess.com/full/6984581.jpg",
-      "name":"Parisian Museum",
-      "date":"2022-11-02",
-      "description":"A unique tour in the city of lights, get to know one of the most iconic places.",
-      "category":"Museum",
-      "place":"Paris",
-      "capacity":8200,
-      "estimate":8200,
-      "price":3500
-    },
-    {
-      _id: 5,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Fiestadedisfraces2.jpg",
-      "name":"Comicon",
-      "date":"2021-02-12",
-      "description":"For comic lovers, all your favourite characters gathered in one place.",
-      "category":"Costume Party",
-      "place":"Room C",
-      "capacity":120000,
-      "assistance":110000,
-      "price":54
-    },
-    {
-      _id: 6,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Fiestadedisfraces1.jpg",
-      "name":"Halloween Night",
-      "date":"2022-02-12",
-      "description":"Come with your scariest costume and win incredible prizes.",
-      "category":"Costume Party",
-      "place":"Room C",
-      "capacity":12000,
-      "estimate":9000,
-      "price":12
-    },
-    {
-      _id: 7,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Conciertodemusica1.jpg",
-      "name":"Metallica in concert",
-      "date":"2022-01-22",
-      "description":"The only concert of the most emblematic band in the world.",
-      "category":"Music Concert",
-      "place":"Room A"
-      ,"capacity":138000,
-      "estimate":138000,
-      "price":150
-    },
-    {
-      _id: 8,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Conciertodemusica2.jpg",
-      "name":"Electronic Fest",
-      "date":"2021-01-22",
-      "description":"The best national and international DJs gathered in one place.",
-      "category":"Music Concert",
-      "place":"Room A",
-      "capacity":138000,
-      "assistance":110300,
-      "price":250
-      },
-    {
-      _id: 9,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Maraton3.jpg",
-      "name":"10K for life",
-      "date":"2021-03-01",
-      "description":"Come and exercise, improve your health and lifestyle.",
-      "category":"Race",
-      "place":"Soccer field",
-      "capacity":30000,
-      "assistance":25698,
-      "price":3
-    },
-    {
-      _id: 10,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Maraton1.jpg",
-      "name":"15K NY",
-      "date":"2022-03-01",
-      "description":"We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-      "category":"Race",
-      "place":"New York",
-      "capacity":3000000,
-      "assistance":2569800,
-      "price":3
-      },
-    {
-      _id: 11,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Libros7.jpg",
-      "name":"School's book fair",
-      "date":"2022-10-15",
-      "description":"Bring your unused school book and take the one you need.",
-      "category":"Book Exchange",
-      "place":"Room D1",
-      "capacity":150000,
-      "estimate":123286,
-      "price":1
-    },
-    {
-      _id: 12,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Libros3.jpg",
-      "name":"Just for your kitchen",
-      "date":"2021-11-09",
-      "description":"If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-      "category":"Book Exchange",
-      "place":"Room D6",
-      "capacity":130000,
-      "assistance":90000,
-      "price":100
-    },
-    {
-      _id: 13,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Cine3.jpg",
-      "name":"Batman",
-      "date":"2021-3-11",
-      "description":"Come see Batman fight crime in Gotham City.",
-      "category":"Cinema",
-      "place":"Room D1",
-      "capacity":11000,
-      "assistance":9300,
-      "price":225
-    },
-    {
-      _id: 14,
-      "image":"https://amazingeventsapi.herokuapp.com/api/img/Cine7.jpg",
-      "name":"Avengers",
-      "date":"2022-10-15",
-      "description":"Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-      "category":"Cinema",
-      "place":"Room D1",
-      "capacity":9000,
-      "estimate":9000,
-      "price":250
-    },
-    
- 
-
-
-  ]
-
- 
+let currentDate = "2022-01-01";
+let events = [
+  {
+    _id: 1,
+    image:
+      "https://i.pinimg.com/originals/ec/f0/c8/ecf0c85e7bef82f981e19b3b52a24caa.jpg",
+    name: "Collectivities Party",
+    date: "2021-12-12",
+    description:
+      "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+    category: "Food Fair",
+    place: "Room A",
+    capacity: 45000,
+    assistance: 42756,
+    price: 5,
+  },
+  {
+    _id: 2,
+    image:
+      "https://i.guim.co.uk/img/media/e83962ba6ef06ff32049c205732bb8057e58ba10/0_201_6071_3645/master/6071.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=e56459f0bf46b483df3721fc3897957c",
+    name: "Korean style",
+    date: "2022-08-12",
+    description:
+      "Enjoy the best Korean dishes, with international chefs and awesome events.",
+    category: "Food Fair",
+    place: "Room A",
+    capacity: 45000,
+    assistance: 42756,
+    price: 10,
+  },
+  {
+    _id: 3,
+    image:
+      "https://s3-us-west-2.amazonaws.com/montana-surveytoolimages/general/full-size/635905468279528880.jpg",
+    name: "Jurassic Park",
+    date: "2021-11-02",
+    description:
+      "Let's go meet the biggest dinosaurs in the paleontology museum.",
+    category: "Museum",
+    place: "Field",
+    capacity: 82000,
+    assistance: 65892,
+    price: 15,
+  },
+  {
+    _id: 4,
+    image: "https://wallpaperaccess.com/full/6984581.jpg",
+    name: "Parisian Museum",
+    date: "2022-11-02",
+    description:
+      "A unique tour in the city of lights, get to know one of the most iconic places.",
+    category: "Museum",
+    place: "Paris",
+    capacity: 8200,
+    estimate: 8200,
+    price: 3500,
+  },
+  {
+    _id: 5,
+    image:
+      "https://www.mcmcomiccon.com/content/dam/sitebuilder/rna/mcm/mcm-london/2021/_img/images/MCM%20Cosplay%2009.jpg/_jcr_content/renditions/original./261428278/MCM%20Cosplay%2009.jpg",
+    name: "Comicon",
+    date: "2021-02-12",
+    description:
+      "For comic lovers, all your favourite characters gathered in one place.",
+    category: "Costume Party",
+    place: "Room C",
+    capacity: 120000,
+    assistance: 110000,
+    price: 54,
+  },
+  {
+    _id: 6,
+    image:
+      "https://todossomosuno.com.mx/portal/wp-content/uploads/2019/10/halloween-dogs.jpg",
+    name: "Halloween Night",
+    date: "2022-02-12",
+    description: "Come with your scariest costume and win incredible prizes.",
+    category: "Costume Party",
+    place: "Room C",
+    capacity: 12000,
+    estimate: 9000,
+    price: 12,
+  },
+  {
+    _id: 7,
+    image:
+      "https://www.revolvermag.com/sites/default/files/media/images/article/metallica_hubbard.jpg",
+    name: "Metallica in concert",
+    date: "2022-01-22",
+    description: "The only concert of the most emblematic band in the world.",
+    category: "Music Concert",
+    place: "Room A",
+    capacity: 138000,
+    estimate: 138000,
+    price: 150,
+  },
+  {
+    _id: 8,
+    image:
+      "https://www.tomorrowland.com/src/Frontend/Themes/tomorrowland/Core/Layout/images/timeline/2019-1.jpg",
+    name: "Electronic Fest",
+    date: "2021-01-22",
+    description:
+      "The best national and international DJs gathered in one place.",
+    category: "Music Concert",
+    place: "Room A",
+    capacity: 138000,
+    assistance: 110300,
+    price: 250,
+  },
+  {
+    _id: 9,
+    image: "https://wallpaper.dog/large/5547139.jpg",
+    name: "10K for life",
+    date: "2021-03-01",
+    description: "Come and exercise, improve your health and lifestyle.",
+    category: "Race",
+    place: "Soccer field",
+    capacity: 30000,
+    assistance: 25698,
+    price: 3,
+  },
+  {
+    _id: 10,
+    image: "https://www.tumejorentrenador.com/wp-content/uploads/2018/09/Free-Download-Running-Wallpaper.jpg",
+    name: "15K NY",
+    date: "2022-03-01",
+    description:
+      "We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
+    category: "Race",
+    place: "New York",
+    capacity: 3000000,
+    assistance: 2569800,
+    price: 3,
+  },
+  {
+    _id: 11,
+    image: "https://wallpaperaccess.com/full/253332.jpg",
+    name: "School's book fair",
+    date: "2022-10-15",
+    description: "Bring your unused school book and take the one you need.",
+    category: "Book Exchange",
+    place: "Room D1",
+    capacity: 150000,
+    estimate: 123286,
+    price: 1,
+  },
+  {
+    _id: 12,
+    image: "https://img.freepik.com/free-photo/chef-cooking-food-restaurant-kitchen_53876-67.jpg",
+    name: "Just for your kitchen",
+    date: "2021-11-09",
+    description:
+      "If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
+    category: "Book Exchange",
+    place: "Room D6",
+    capacity: 130000,
+    assistance: 90000,
+    price: 100,
+  },
+  {
+    _id: 13,
+    image: "https://static4.abc.es/media/play/2020/08/21/liga-justicia-batman-kO4H--1200x630@abc.jpg",
+    name: "Batman",
+    date: "2021-3-11",
+    description: "Come see Batman fight crime in Gotham City.",
+    category: "Cinema",
+    place: "Room D1",
+    capacity: 11000,
+    assistance: 9300,
+    price: 225,
+  },
+  {
+    _id: 14,
+    image: "https://images4.alphacoders.com/909/thumb-1920-909185.jpg",
+    name: "Avengers",
+    date: "2022-10-15",
+    description:
+      "Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
+    category: "Cinema",
+    place: "Room D1",
+    capacity: 9000,
+    estimate: 9000,
+    price: 250,
+  },
+];
